87fde256e6f57b34549d682e66c4c58cb494cb06
Murashko91 <Murashko.s.v91@gmail.com>
2024-04-25 14:35:01 +0300

    Update top.go

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,34 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
+import "strings"
+
+func Top10(input string) []string {
+
+	words := strings.Fields(input)
+
+	wordsCountMap := make(map[string]int)
+
+	for _, word := range words {
+
+		value, isPresent := wordsCountMap[word]
+
+		if isPresent {
+			wordsCountMap[word] = value + 1
+		} else {
+			wordsCountMap[word] = 1
+		}
+	}
+
 	return nil
+}
+
+func sortTop10(wordsCountMap map[string]int) []string {
+
+	result := make([]string, 0, 10)
+
+	for i := 0; i < 10; i++ {
+
+	}
+	return nil
+
 }

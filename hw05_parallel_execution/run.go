851d0e297b7e1a34ee0a7500f94971b68e08c402
Murashko91 <Murashko.s.v91@gmail.com>
2024-05-16 12:06:59 +0300

    Added unit test

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -22,27 +22,35 @@ func Run(tasks []Task, n, m int) error {
 
 	wg := &sync.WaitGroup{}
 	wc := &workContext{tasks: tasks, mu: &sync.Mutex{}, wg: wg}
+	errorLimitEnabled := m > 0
+
 	for i := 0; i < n; i++ {
 
 		wg.Add(1)
-		go doWork(wc, m)
+		go doWork(wc, m, errorLimitEnabled)
 	}
 	wg.Wait()
 
-	if wc.errorsNum >= m {
+	if wc.errorsNum >= m && errorLimitEnabled {
 		return ErrErrorsLimitExceeded
 	}
 
 	return nil
 }
 
-func doWork(wc *workContext, m int) {
+func doWork(wc *workContext, maxErr int, errorLimitEnabled bool) {
 
 	for {
 
 		wc.mu.Lock()
 
-		errLimit := wc.errorsNum >= m
+		errLimit := wc.errorsNum >= maxErr
+
+		// Desable error limit validation for m <= 0
+		if !errorLimitEnabled {
+			errLimit = false
+		}
+
 		taskIndex := wc.tNumber
 		tasksCompleated := taskIndex > len(wc.tasks)-1
 
@@ -57,7 +65,7 @@ func doWork(wc *workContext, m int) {
 		// Execute task
 		err := wc.tasks[taskIndex]()
 
-		if err != nil {
+		if err != nil && errorLimitEnabled {
 			wc.mu.Lock()
 			wc.errorsNum++
 			wc.mu.Unlock()
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -39,6 +39,29 @@ func TestRun(t *testing.T) {
 		require.LessOrEqual(t, runTasksCount, int32(workersCount+maxErrorsCount), "extra tasks were started")
 	})
 
+	t.Run("if were errors but M is 0 - all tasks should be", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.NoError(t, err)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were started")
+	})
+
 	t.Run("tasks without errors", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
@@ -75,12 +98,11 @@ func TestRun(t *testing.T) {
 		workersCount := 5
 		maxErrorsCount := 1
 
-		var executionState = &struct {
+		executionState := &struct {
 			i int
 		}{}
 
 		for i := 0; i < tasksCount; i++ {
-
 			tasks = append(tasks, func() error {
 				executionState.i++
 

2c390921fd1d058efee83895d0087b57e784a960
Murashko91 <Murashko.s.v91@gmail.com>
2024-05-28 22:09:20 +0300

    hw07_file_copying initial commit

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,24 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("Test ErrUnsupportedFile", func(t *testing.T) {
+		tmpFile, _ := os.CreateTemp(".", "tmpFile.txt")
+		outFileName := "outFile.txt"
+
+		defer func() {
+			tmpFile.Close()
+			os.Remove(tmpFile.Name())
+		}()
+
+		err := Copy(tmpFile.Name(), outFileName, 0, 0)
+
+		require.Equal(t, err, ErrUnsupportedFile)
+	})
 }

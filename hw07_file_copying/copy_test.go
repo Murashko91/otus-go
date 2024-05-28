cadb428252456daa2081c37b2b2cfde1c86a8090
Murashko91 <Murashko.s.v91@gmail.com>
2024-05-28 22:52:34 +0300

    Added positive test

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+const outFileName, tmpFileName = "outFile.txt", "tmpFile.txt"
+
 func TestCopy(t *testing.T) {
 	t.Run("Test ErrUnsupportedFile", func(t *testing.T) {
-		tmpFile, _ := os.CreateTemp(".", "tmpFile.txt")
-		outFileName := "outFile.txt"
+		tmpFile, _ := os.CreateTemp(".", tmpFileName)
 
 		defer func() {
 			tmpFile.Close()
@@ -23,12 +26,15 @@ func TestCopy(t *testing.T) {
 
 		_, outFileErr := os.Stat(outFileName)
 
+		if outFileErr == nil {
+			os.Remove(outFileName)
+		}
+
 		require.Equal(t, true, os.IsNotExist(outFileErr))
 	})
 
 	t.Run("Test ErrOffsetExceedsFileSize", func(t *testing.T) {
-		tmpFile, _ := os.CreateTemp(".", "tmpFile.txt")
-		outFileName := "outFile.txt"
+		tmpFile, _ := os.CreateTemp(".", tmpFileName)
 		tmpFile.WriteString("1")
 
 		defer func() {
@@ -40,8 +46,39 @@ func TestCopy(t *testing.T) {
 
 		_, outFileErr := os.Stat(outFileName)
 
+		if outFileErr == nil {
+			os.Remove(outFileName)
+		}
+
 		require.Equal(t, true, os.IsNotExist(outFileErr))
 
 		require.Equal(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("Test Positive test", func(t *testing.T) {
+		tmpFile, _ := os.CreateTemp(".", tmpFileName)
+		tmpFile.WriteString("12345")
+
+		defer func() {
+			tmpFile.Close()
+			os.Remove(tmpFile.Name())
+		}()
+
+		err := Copy(tmpFile.Name(), outFileName, 2, 1)
+
+		_, outFileErr := os.Stat(outFileName)
+
+		outFile, _ := os.Open(outFileName)
+
+		data, _ := io.ReadAll(outFile)
+
+		fmt.Println(string(data) == "3")
+
+		if outFileErr == nil {
+			os.Remove(outFileName)
+		}
+
+		require.Equal(t, string(data), "3")
+		require.Equal(t, err, nil)
 	})
 }

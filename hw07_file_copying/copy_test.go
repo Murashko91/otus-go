6cdb1f9393755776d6d186fbd98ca4c1635e68bb
Murashko91 <Murashko.s.v91@gmail.com>
2024-05-28 22:37:15 +0300

    Update copy_test.go

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -20,5 +20,28 @@ func TestCopy(t *testing.T) {
 		err := Copy(tmpFile.Name(), outFileName, 0, 0)
 
 		require.Equal(t, err, ErrUnsupportedFile)
+
+		_, outFileErr := os.Stat(outFileName)
+
+		require.Equal(t, true, os.IsNotExist(outFileErr))
+	})
+
+	t.Run("Test ErrOffsetExceedsFileSize", func(t *testing.T) {
+		tmpFile, _ := os.CreateTemp(".", "tmpFile.txt")
+		outFileName := "outFile.txt"
+		tmpFile.WriteString("1")
+
+		defer func() {
+			tmpFile.Close()
+			os.Remove(tmpFile.Name())
+		}()
+
+		err := Copy(tmpFile.Name(), outFileName, 2, 0)
+
+		_, outFileErr := os.Stat(outFileName)
+
+		require.Equal(t, true, os.IsNotExist(outFileErr))
+
+		require.Equal(t, err, ErrOffsetExceedsFileSize)
 	})
 }

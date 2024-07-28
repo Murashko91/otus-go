5e3cdba970a14b59c1ce13aa10f1e72efde5a0e2
Murashko91 <Murashko.s.v91@gmail.com>
2024-07-28 17:41:40 +0300

    Added logic for main

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,26 +1,83 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
 func main() {
-	var protocol, host, port string
-	for i, arg := range os.Args {
-		switch i {
-		case 1:
-			protocol = arg
-		case 2:
-			host = arg
-		case 3:
-			port = arg
-		}
+	i := 1
+	if len(os.Args) > 3 {
+		i = 2
+	}
+	if len(os.Args) < 3 {
+		panic("Wrong args")
+	}
+	timeoutString := flag.String("timeout", "10s", "usage string")
+	flag.Parse()
+	host := os.Args[i]
+	port := os.Args[i+1]
 
-		fmt.Println(protocol, port, host)
-
+	timeout, err := time.ParseDuration(*timeoutString)
+	if err != nil {
+		panic(err.Error())
 	}
 
+	client := NewTelnetClient(fmt.Sprintf("%s:%s", host, port), timeout, os.Stdin, os.Stdout)
+	err = client.Connect()
+	if err != nil {
+		fmt.Println("Connection error")
+		fmt.Println(err)
+		return
+	}
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	go listenSignals(client, wg)
+	go listenFromClient(client, wg)
+	go sendToClient(client, wg)
+	go forceTimeout(timeout, wg)
+
+	wg.Wait()
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+}
+
+func listenSignals(client TelnetClient, wg *sync.WaitGroup) {
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, syscall.SIGINT)
+
+	<-signalChannel
+	err := client.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	wg.Done()
+}
+
+func listenFromClient(client TelnetClient, wg *sync.WaitGroup) {
+	err := client.Receive()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	wg.Done()
+}
+
+func sendToClient(client TelnetClient, wg *sync.WaitGroup) {
+	err := client.Send()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	client.Close()
+	wg.Done()
+}
+
+func forceTimeout(timeout time.Duration, wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(timeout)
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"io"
 	"net"
 	"time"
@@ -19,35 +20,34 @@ type TelnetClientImpl struct {
 	timeout time.Duration
 	in      io.ReadCloser
 	out     io.Writer
+	Ctx     context.Context
+	cancel  context.CancelFunc
 }
 
 func (t *TelnetClientImpl) Connect() error {
-	conn, err := net.Dial("tcp", t.address)
-	if err != nil {
-		return err
-	}
-	t.conn = conn
-
-	return nil
-
+	var err error
+	dialer := &net.Dialer{}
+	t.Ctx, t.cancel = context.WithTimeout(context.Background(), t.timeout)
+	t.conn, err = dialer.DialContext(t.Ctx, "tcp", t.address)
+	return err
 }
 
 func (t *TelnetClientImpl) Close() error {
+	t.cancel()
 	return t.conn.Close()
 }
 
 func (t *TelnetClientImpl) Send() error {
-	io.Copy(t.conn, t.in)
-	return nil
+	_, err := io.Copy(t.conn, t.in)
+	return err
 }
 
 func (t *TelnetClientImpl) Receive() error {
-	io.Copy(t.out, t.conn)
-	return nil
+	_, err := io.Copy(t.out, t.conn)
+	return err
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-
 	return &(TelnetClientImpl{
 		address: address,
 		timeout: timeout,

f029e96120723001dced07bb6eaba6bbd175fa3e
Murashko91 <Murashko.s.v91@gmail.com>
2024-08-08 20:44:59 +0300

    added config

--- hw12_13_14_15_calendar/cmd/calendar/config.go
+++ hw12_13_14_15_calendar/cmd/calendar/config.go
@@ -1,20 +1,38 @@
 package main
 
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
 // При желании конфигурацию можно вынести в internal/config.
 // Организация конфига в main принуждает нас сужать API компонентов, использовать
 // при их конструировании только необходимые параметры, а также уменьшает вероятность циклической зависимости.
 type Config struct {
-	Logger LoggerConf
+	Logger LoggerConf `yaml:"logger"`
 	// TODO
 }
 
 type LoggerConf struct {
-	Level string
+	Level string `yaml:"level"`
 	// TODO
 }
 
-func NewConfig() Config {
-	return Config{}
+func NewConfig(configFilePath string) Config {
+	conf := &Config{}
+
+	file, err := os.Open(configFilePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	decoder := yaml.NewDecoder(file)
+	if err = decoder.Decode(conf); err != nil {
+		panic(err)
+	}
+
+	return *conf
 }
 
 // TODO

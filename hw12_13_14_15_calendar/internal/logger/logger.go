692e32ecb07a16a4e1d7949a51491f4c9ac061c1
Murashko91 <Murashko.s.v91@gmail.com>
2024-08-09 09:29:50 +0300

    Update logger.go

--- hw12_13_14_15_calendar/internal/logger/logger.go
+++ hw12_13_14_15_calendar/internal/logger/logger.go
@@ -1,20 +1,49 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type Logger struct { // TODO
+var logLevelsMap = map[string]int{
+	"error": 1,
+	"warn":  2,
+	"info":  3,
+	"debug": 4,
+}
+
+type Logger struct {
+	level int
 }
 
 func New(level string) *Logger {
-	return &Logger{}
+	logLevel, isListed := logLevelsMap[level]
+
+	if !isListed {
+		fmt.Printf("loglevel %s is not supported. supported levels\n: %v", level, logLevelsMap)
+	}
+	return &Logger{level: logLevel}
+}
+
+func (l Logger) Debug(msg string) {
+	l.log(msg, logLevelsMap["debug"])
 }
 
 func (l Logger) Info(msg string) {
-	fmt.Println(msg)
+	l.log(msg, logLevelsMap["info"])
+}
+
+func (l Logger) Warn(msg string) {
+	l.log(msg, logLevelsMap["warn"])
+
 }
 
 func (l Logger) Error(msg string) {
-	// TODO
+	l.log(msg, logLevelsMap["error"])
+
 }
 
-// TODO
+func (l Logger) log(msg string, logerLevel int) {
+	if l.level >= logerLevel {
+		fmt.Println(msg)
+	}
+}

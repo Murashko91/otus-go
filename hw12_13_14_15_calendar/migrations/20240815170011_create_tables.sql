db407fb32e0c752fd8480053f196238d1619d026
Murashko91 <Murashko.s.v91@gmail.com>
2024-08-26 17:22:51 +0300

    Update 20240815170011_create_tables.sql

--- hw12_13_14_15_calendar/migrations/20240815170011_create_tables.sql
+++ hw12_13_14_15_calendar/migrations/20240815170011_create_tables.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS events (
   id SERIAL PRIMARY KEY,
   user_id INT,
   title TEXT NOT NULL,
-  event_date  TIMESTAMP NOT NULL,
-	last_modified_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  descr TEXT,
+  start_date  TIMESTAMP NOT NULL,
+  end_date  TIMESTAMP NOT NULL
 	);
 
 --INSERT INTO events (title, event_date, user_id) VALUES ( 'Some event', '2024-08-14 16:50:36', '1');
